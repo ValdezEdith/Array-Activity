17b4085ade81e0fd9d4692f8325b18eda1276e32
ValdezEdith <zedlavhtide@gmail.com>
2024-10-16 20:23:39 +0800

    Create arrayActiviry.js

---
+++ arrayActiviry.js
@@ -0,0 +1,17 @@
+// create favoritsinger in an array
+const favSingers = ["Taylor Swift", "Olivia Rodrigo", "Jungkook"];
+
+// Log the first singer in the array using array index notation.
+console.log(favSingers[0]);
+
+// Create an array called favNumbers and store four of your favorite numbers.
+const favNumbers = [1, 2, 3, 4];
+
+// Create an array named mixedArr and store different data types: ["string", ["otherarray"], 123, true).
+const mixedArr = ["string", ["otherarray"], 123, true]; 
+
+// Access and log each item in the mixedArr using array index notation.
+console.log(mixedArr[0]); // "string"
+console.log(mixedArr[1]); // ["otherarray"]
+console.log(mixedArr[2]); // 123
+console.log(mixedArr[3]); // true
